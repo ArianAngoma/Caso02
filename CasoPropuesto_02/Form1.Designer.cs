74dfb49c9907153f6364cff41807f872c4b63497
angoma850 <arian.angoma@tecsup.edu.pe>
2019-08-12 22:08:05 -0500

    first commit

---
+++ CasoPropuesto_02/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace CasoPropuesto_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dg01 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cb01 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dg01)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dg01
+            // 
+            this.dg01.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg01.Location = new System.Drawing.Point(42, 97);
+            this.dg01.Name = "dg01";
+            this.dg01.Size = new System.Drawing.Size(710, 274);
+            this.dg01.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "REPORTE DE PEDIDOS POR CLIENTE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "NOMBRE DEL CLIENTE";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(634, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "CONSULTAR";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(545, 397);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "TOTAL DE PEDIDOS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(710, 397);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "TOTAL";
+            // 
+            // cb01
+            // 
+            this.cb01.FormattingEnabled = true;
+            this.cb01.Location = new System.Drawing.Point(193, 50);
+            this.cb01.Name = "cb01";
+            this.cb01.Size = new System.Drawing.Size(384, 21);
+            this.cb01.TabIndex = 7;
+            this.cb01.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cb01);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dg01);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg01)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dg01;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cb01;
+    }
+}
+
---
+++ CasoPropuesto_02/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CasoPropuesto_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["CasoPropuesto_02"].ConnectionString);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListaBox();
+            ListaClientes();
+        }
+
+        public void ListaClientes()
+        {
+
+            using (SqlDataAdapter df = new SqlDataAdapter("Usp_Caso_Segundo", cn))
+            {
+                df.SelectCommand.CommandType = CommandType.StoredProcedure;
+                using (DataSet Da = new DataSet())
+                {
+                    df.Fill(Da, "Pedidos");
+                    dg01.DataSource = Da.Tables["Pedidos"];
+                }
+            }
+        }
+
+        public void ListaBox()
+        {
+            using (SqlDataAdapter df = new SqlDataAdapter("Usp_Caso_Segundo_Box", cn))
+            {
+                df.SelectCommand.CommandType = CommandType.StoredProcedure;
+                using (DataSet Da = new DataSet())
+                {
+                    df.Fill(Da, "Pedidos");
+                    var datos = Da.Tables["Pedidos"];
+                    cb01.DataSource = Da.Tables["Pedidos"];
+                    cb01.ValueMember = "IdEmpleado";
+                    cb01.DisplayMember = "IdEmpleado";
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
